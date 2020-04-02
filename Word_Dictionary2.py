1cde1e889114d43d6884995ac34cda683f8e1b14
dnguyen773 <danny.nguyen88@yahoo.com>
2020-04-02 15:24:04 -0700

    Add files via upload

---
+++ Word_Dictionary2.py
@@ -0,0 +1,36 @@
+import json
+import difflib
+#!/usr/bin/env python
+data = json.load(open("/Users/dannynguyen/PycharmProjects/files/data.json"))
+
+def word_finder(w):
+    if w in data:
+        return data[w]
+    elif len(difflib.get_close_matches(w,data.keys()))> 0:
+        user_input = input("Did you mean %s instead? Enter N or Y "% difflib.get_close_matches(w,data.keys())[0])
+        if user_input.upper() =="Y":
+             return data[difflib.get_close_matches(w,data.keys())[0]]
+        elif user_input.upper() =="N":
+            return "Sorry, the word doesn't exist. Please double check it"
+        else:
+            return "Sorry but we don't understand your entry.."
+    else:
+        return "The word doesn't exist"
+
+
+print("Welcome to the English Dictionary!")
+while True:
+    word = input("Enter a word you would like to look up.\n ")
+    if word == "stop":
+        break
+    else:
+
+        # print(word_finder(word))
+        output = word_finder(word.lower())
+        if type(output) == list:
+            increment = 1
+            for item in output:
+                print(increment, ":" , item)
+                increment += 1
+        else:
+            print(output)
